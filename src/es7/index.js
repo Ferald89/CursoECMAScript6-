4a9a86e99911c5a744f9bea525338b1a876859af
Fernando Aldair <ferpool_ald@hotmail.com>
2020-04-18 13:29:23 -0500

    Se agregaron las caracteristicas es7

---
+++ src/es7/index.js
@@ -0,0 +1,14 @@
+//Vamos a buscar como encontrar un número en un areglo
+let numbers = [1,2,3,4,5,6,7]
+if (numbers.includes(11)){
+    console.log("Si se encuentra el valor")
+} else {
+    console.log("No se encuenta el valor")
+}
+
+let base = 4 
+let exponent = 4
+
+let result = base **exponent // De esta forma se puede acceder a l exponente
+
+console.log(result)
