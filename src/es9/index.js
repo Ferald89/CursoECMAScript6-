ad38969318bd3e8eafd9d0f6a8a62294af6dff1b
Fernando Aldair <ferpool_ald@hotmail.com>
2020-04-19 09:38:50 -0500

    es9

--- src/es9/index.js
+++ src/es9/index.js
@@ -9,3 +9,38 @@ const obj = {
 
 let {name ,...all} = obj
 console.log(name,all)
+
+const obj1 = {
+    name : 'Abrham',
+    age : 32,
+}
+
+const obj2 = {
+    ...obj1,
+    country : 'MX'
+}
+
+console.log(obj2)
+
+const helloWorld = () =>{
+    return new Promise((resolve,reject) =>{
+        (true)
+        ? setTimeout( () => resolve('Hello world'),3000)
+        : reject(new Error('Test Error'))
+    })
+}
+
+helloWorld()
+       .then(response =>console.log(response))
+       .catch(error => console.log(error))
+       .finally(() => console.log('Finalizo'))
+
+
+
+const regextData = /([0-9]{4}-[0-9]{2}-[0-9]{2})/
+const match =regextData.exec('2018-04-09')
+const year = match[1]
+const month = match[2]
+const day = match[3]
+
+console.log(year,month,day)
