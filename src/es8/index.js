8217eb86dbdb0dff5cf364a1d87e736de8a496e8
Fernando Aldair <ferpool_ald@hotmail.com>
2020-04-19 08:29:49 -0500

    SE agrego es8

---
+++ src/es8/index.js
@@ -0,0 +1,26 @@
+
+const data = {
+    //inicia objeto
+    frontend:'Oscar',
+    backend : 'ISabel',
+    design : 'Ana'
+}
+
+const entries = Object.entries(data)
+console.log(entries)
+console.log(entries.length)
+//Obtiene los valores del objeto
+const data = {
+    //inicia objeto
+    frontend:'Oscar',
+    backend : 'ISabel',
+    design : 'Ana'
+}
+
+const values =Object.values(data)
+console.log(values)
+
+//padis
+const string = 'hello'
+console.log(string.padStart(1,'hi'));
+
